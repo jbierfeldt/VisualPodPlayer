5719011236177a5cf58e5cdd1d49691cc00ddf64
Jackson Bierfeldt <jbierfeldt@gmail.com>
2018-05-10 10:10:02 +0200

    Cleaned up electron-starter.js

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -120,7 +120,6 @@ function createWindow () {
   });
   mainWindow.loadURL(startUrl);
 
-    // menu.popup();
   Menu.setApplicationMenu(menu)
 
   // Open the DevTools.
@@ -156,26 +155,3 @@ app.on('activate', function () {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
-// Create the Application's main menu
-// const template = [{
-//     label: "Application",
-//     submenu: [
-//         { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-//         { type: "separator" },
-//         { label: "Quit", accelerator: "CmdOrCtrl+Q", click: function() { app.quit(); }}
-//     ]}, {
-//     label: "Edit",
-//     submenu: [
-//         { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-//         { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-//         { type: "separator" },
-//         { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-//         { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-//         { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-//         { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-//     ]}
-// ];
